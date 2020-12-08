9a31b49ee1c414d6c6dda5580b85619e6765faff
xec-cm <fcatala@irsicaixa.es>
2020-12-08 11:24:46 +0100

    day 1 question 2

--- R/01_adventofcode.r
+++ R/01_adventofcode.r
@@ -1,13 +1,20 @@
 # Advent of code 2020
 # Francesc Català-Moll
 
+library(tidyverse)
+library(glue)
+
+# Part 1 ------------------------------------------------------------------
+
 # In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying
 # them together produces 1721 * 299 = 514579, so the correct answer is 514579.
 #
 # Of course, your expense report is much larger. Find the two entries that sum
 # to 2020; what do you get if you multiply them together?
 
-library(tidyverse)
+
+## =========================
+## Option 1
 
 dat <- read_tsv("data/inputdata_01.txt", col_names = "values") %>% pull()
 
@@ -19,8 +26,53 @@ for (it in seq_along(dat)) {
     if (length(inters) == 0) {
         checked[it] <- dat[it]
     } else {
-        print(glue::glue("result: {dat[it]} + {inters} = {dat[it] + inters}"))
-        print(glue::glue("result: {dat[it]} * {inters} = {dat[it] * inters}"))
+        print(glue("result: {dat[it]} + {inters} = {dat[it] + inters}"))
+        print(glue("result: {dat[it]} * {inters} = {dat[it] * inters}"))
+        break
+    }
+}
+
+rm(list = ls())
+
+## =========================
+## Option 2
+
+dat <- read_tsv("data/inputdata_01.txt", col_names = "values") %>% pull()
+for (it in seq_along(dat)) {
+    diff <- 2020 - dat[it]
+    if (diff %in% dat) {
+        print(glue("result: {dat[it] * diff}" ))
+        break
+    }
+}
+
+rm(list = ls())
+
+# Part 2 ------------------------------------------------------------------
+
+# Using the above example again, the three entries that sum to 2020 are 979, 366,
+# and 675. Multiplying them together produces the answer, 241861950. In your
+# expense report, what is the product of the three entries that sum to 2020?
+
+library(tidyverse)
+
+dat <- read_tsv("data/inputdata_01.txt", col_names = "values") %>% pull()
+
+for (it in seq_along(dat)) {
+    sum <- dat + dat[it]
+    sum <- sum[sum < 2020]
+
+    for (it2 in seq_along(sum)) {
+        diff <- 2020 - sum[it2]
+        if (diff %in% dat) {
+            r <- c(dat[it], sum[it2] - dat[it], diff)
+            break
+        }
+    }
+
+    if (exists("r")) {
+        print(glue("result: {v1} + {v2} + {v3} = {v1 + v2 + v3}"))
+        print(glue("result: {v1} * {v2} * {v3} = {v1 * v2 * v3}"))
         break
     }
 }
